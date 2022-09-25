2a03d70792fbb2c8cc2a41ef8abb8d9e6e834e37
Samarth H Chinivar <samarthharish321@gmail.com>
2022-09-25 22:27:05 +0530

    Resume 3-0 added

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -31,7 +31,7 @@ const About = () => {
             </article>
           </div>
 
-          <p>I would like to solve various real-life problems and to explore these ever-evolving technologies. I am aiming to get an opportunity to work in an organisation where I can make the best use of my potential and contribute to the organization's growth.</p>
+          <p>I am Samarth H Chinivar from Bengaluru, India. My current ambition is to solve various real-life problems and to explore these ever-evolving technologies. I am aiming to get an opportunity to work in an organisation where I can make the best use of my potential and contribute to the organization's growth.</p>
 
           <a href="#contact" className='btn btn-primary'>Let's Talk</a>
 
